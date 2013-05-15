c36ed0799f08a3ca97f477a81c7a4f9020b509cb
Antoine <antoine.dutot@gmail.com>
2013-05-15 14:04:12 +0200

    Adapted the android code for the changes in libraries.
    - Added a AndroidArmatureLoader.

--- android/src/org/sofa/opengl/backend/AndroidArmatureLoader.scala
+++ android/src/org/sofa/opengl/backend/AndroidArmatureLoader.scala
@@ -1,12 +1,14 @@
 package org.sofa.opengl.backend
 
-import org.sofa.opengl.simu.oberon.renderer.{Armature, ArmatureLoader, SVGArmatureLoader}
+import android.content.res.Resources
+import org.sofa.backend.AndroidLoader
+import org.sofa.simu.oberon.renderer.{Armature, ArmatureLoader, SVGArmatureLoader}
 
 /** Default loader for shaders, based on files and the include path.
   * This loader tries to open the given resource directly, then if not
   * found, tries to find it in each of the pathes provided by the include
   * path. If not found it throws an IOException. */
-class AndroidArmatureLoader extends AndroidLoader with ArmatureLoader {
+class AndroidArmatureLoader(val resources:Resources) extends AndroidLoader with ArmatureLoader {
 	private[this] val SVGLoader = new SVGArmatureLoader()
 
     def open(name:String, texRes:String, shaderRes:String, resource:String):Armature = {
