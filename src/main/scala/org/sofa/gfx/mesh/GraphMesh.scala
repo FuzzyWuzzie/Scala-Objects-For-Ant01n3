e77f45137445439722be3301b275da778b627019
Antoine Dutot <antoine.dutot@litislab.eu>
2014-12-11 17:16:48 +0100

    Allow to remove nodes and edges in GraphMesh.

--- src/main/scala/org/sofa/gfx/mesh/GraphMesh.scala
+++ src/main/scala/org/sofa/gfx/mesh/GraphMesh.scala
@@ -6,9 +6,13 @@ import org.sofa.nio.{IntBuffer}
 
 
 /** A data structure suitable to represent a graph or part of a graph.
-  * @param nodeCount The max number of nodes.
-  * @param edgeCount The max number of edges. */
-class GraphMesh(val nodeCount:Int, val edgeCount:Int) extends MultiMesh {
+  * @param maxNodes The max number of nodes.
+  * @param maxEdges The max number of edges. */
+class GraphMesh(val maxNodes:Int, val maxEdges:Int) extends MultiMesh {
+
+	protected[this] var nodeCount = 0
+
+	protected[this] var edgeCount = 0
 
 	/** The mutable set of coordinates. */
 	protected[this] val V:MeshAttribute = addMeshAttribute(VertexAttribute.Vertex, 3)
@@ -17,14 +21,14 @@ class GraphMesh(val nodeCount:Int, val edgeCount:Int) extends MultiMesh {
 	protected[this] lazy val C:MeshAttribute = addMeshAttribute(VertexAttribute.Color, 4)
 	
 	/** The mutable set of nodes. */
-	protected[this] val nodes:MeshElement = new MeshElement(nodeCount, elementsPerPrimitive(0))
+	protected[this] val nodes:MeshElement = new MeshElement(maxNodes, elementsPerPrimitive(0))
 
 	/** The mutable set of edges. */
-	protected[this] val edges:MeshElement = new MeshElement(edgeCount, elementsPerPrimitive(1))
+	protected[this] val edges:MeshElement = new MeshElement(maxEdges, elementsPerPrimitive(1))
     	
 	// -- Mesh interface -----------------------------------------------------
 
-	def vertexCount:Int = nodeCount * elementsPerPrimitive(0)
+	def vertexCount:Int = maxNodes * elementsPerPrimitive(0)
 
     def elementsPerPrimitive(subMesh:Int):Int = subMesh match {
     	case 0 => 1
@@ -75,21 +79,84 @@ class GraphMesh(val nodeCount:Int, val edgeCount:Int) extends MultiMesh {
     	if(c+1 > C.end) C.end = c + 1
     }
 
-	def node(n:Int, vertex:Int) {
-    	nodes.theData(n) = vertex
+	def addNode():Int = {
+		val n = nodeCount
+    	nodes.theData(n) = n  		// Yes actually the nodes array is pretty not useful...
 
     	if(n   < nodes.beg) nodes.beg = n
     	if(n+1 > nodes.end) nodes.end = n + 1
+    
+    	nodeCount += 1
+    	n
     }
 
-    def edge(e:Int, vertexFrom:Int, vertexTo:Int) {
+    def addEdge(from:Int, to:Int):Int = {
+    	val e = edgeCount
     	val i = e * 2
 
-    	edges.theData(i+0) = vertexFrom
-    	edges.theData(i+1) = vertexTo
+    	edges.theData(i+0) = from
+    	edges.theData(i+1) = to
 
     	if(e   < edges.beg) edges.beg = e
     	if(e+1 > edges.end) edges.end = e + 1
+    
+    	edgeCount += 1
+    	e
+    }
+
+    def delNode(n:Int) {
+    	nodeCount -= 1
+
+    	if(n < nodeCount) {
+    		// There is nothing to do with the nodes !
+    		// They always stay in the same order, however
+    		// the attribute data changes.
+    		delVertex(n)
+    		delColor(n)
+    	}
+    }
+
+    protected def delVertex(n:Int) {
+    	val d = V.theData
+    	val l = nodeCount * 3
+    	val i = n * 3
+
+    	d(i+0) = d(l+0)
+    	d(i+1) = d(l+1)
+    	d(i+2) = d(l+2)
+
+    	if(n   < V.beg) V.beg = n
+    	if(n+1 > V.end) V.end = n + 1
+    }
+
+    protected def delColor(n:Int)  {
+    	val d = C.theData
+    	val l = nodeCount * 4
+    	val i = n * 4
+
+    	d(i+0) = d(l+0)
+    	d(i+1) = d(l+1)
+    	d(i+2) = d(l+2)
+    	d(i+3) = d(l+3)
+
+    	if(n   < C.beg) C.beg = n
+    	if(n+1 > C.end) C.end = n + 1    	
+    }
+
+    def delEdge(e:Int) {
+    	edgeCount -= 1
+
+    	if(e < edgeCount) {
+    		val src = edgeCount * 2
+    		val dst = e * 2
+    		val d   = edges.theData
+
+    		d(dst+0) = d(src+0)
+    		d(dst+1) = d(src+1)
+
+    		if(dst   < edges.beg) edges.beg = dst
+    		if(dst+1 > edges.end) edges.end = dst + 1
+   		}
     }
 
 	// -- Dynamic updating -----------------------------------------------------
@@ -115,5 +182,15 @@ class GraphMesh(val nodeCount:Int, val edgeCount:Int) extends MultiMesh {
 			if(updateNodes)    nodes.update(va)
 			if(updateEdges)    edges.update(va)
 		}
+	}
+
+	override def toString():String = {
+		"mesh(%d, %d)%n  nodes(%s)%n  edges(%s)%n  vertex(%s)%n  colors(%s)%n".format(
+			nodeCount, edgeCount,
+			nodes.theData.mkString(", "),
+			edges.theData.mkString(", "),
+			V.theData.mkString(", "),
+			C.theData.mkString(", ")
+		)
 	}
 }
